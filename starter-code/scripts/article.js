84be64261cce3ebfc183d829fbdd429fa2fbb15e
Davidchhing <davidhchhing@gmail.com>
2018-05-08 20:45:29 -0700

    cached info to local storage

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -51,7 +51,6 @@ Article.fetchAll = function() {
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
     var parsed = JSON.parse(localStorage.rawData);
-    console.log(parsed);
     Article.loadAll(parsed); //TODONE: What do we pass in to loadAll()? 
     articleView.initIndexPage();    
     //TODONE: What method do we call to render the index page?
@@ -62,21 +61,16 @@ Article.fetchAll = function() {
         url:'../data/hackerIpsum.json',
         data: "data",
         success: function (data) {
-        
-          console.log(data);
-          localStorage.setItem("rawData", JSON.stringify(data));
-         
+        localStorage.setItem("rawData", JSON.stringify(data));
         }
       });
-      
       articleView.initIndexPage();
-      
        };
-       
-    // TODO: When we don't already have the rawData,
+  }
+    // TODONE: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-  }
+
 
